351c7c975e24bd9e06ba106d87e3670a565a5088
Imran Hossen <imranhossen20@gmail.com>
2022-04-18 22:05:20 +0600

    Header part done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './Components/About/About';
 import Home from './Components/Home/Home';
 import Footer from './Components/Shared/Footer/Footer';
 import Header from './Components/Shared/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
